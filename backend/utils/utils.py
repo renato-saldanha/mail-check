a8e92230d0873db53b7e5a3255155beb86d5183d
Renato Saldanha <ranalisesaldanha@gmail.com>
2026-01-17 21:01:55 -0400

    ajuste de sintaxe

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -59,7 +59,7 @@ def get_llm():
 
     llm = ChatGoogleGenerativeAI(
         model="gemini-2.5-flash",
-        temperature="0.2",
+        temperature=0.2,
         # max_output_tokens=MAX_TOKENS,
     )
 
